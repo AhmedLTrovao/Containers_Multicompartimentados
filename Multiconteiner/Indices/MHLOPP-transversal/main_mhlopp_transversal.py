86d30a149a4689265f97739eed982a35ee5fa46e
Ahmed Lima Trovão <145933054+AhmedLTrovao@users.noreply.github.com>
2026-05-01 16:19:44 -0300

    26/04

--- Multiconteiner/Indices/MHLOPP-transversal/main_mhlopp_transversal.py
+++ Multiconteiner/Indices/MHLOPP-transversal/main_mhlopp_transversal.py
@@ -7,10 +7,10 @@ from compile_results import compilar_resultados
 if __name__ == "__main__":
     # 1. Configurações de Caminhos
     # Use o r"" para evitar problemas com as barras invertidas do Windows
-    arquivo_instancias = r"C:\Users\progo\Containers_Multicompartimentados\Multiconteiner\Indices\MHLOPP-transversal\DATA_1_m05b2d1\DATA_1_n10m05b2d1.dat"
+    arquivo_instancias = r"C:\Users\progo\Containers_Multicompartimentados\Multiconteiner\Indices\MHLOPP-transversal\DATA_1_m05b2d1\DATA_1_n30m05b2d1.dat"
     
     # Pasta onde todos os resultados (txt e resumo) serão salvos
-    pasta_saida = "resultados_n10m05b2d1"
+    pasta_saida = "resultados_n30m05b2d1"
     os.makedirs(pasta_saida, exist_ok=True)
 
     start_time_geral = time.time()
